384a3d488b1e2ffacebae026125880f33490fb02
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-10 21:51:13 +0200

    Clean Up Fetch Users

--- src/components/context/github/GithubReducer.js
+++ src/components/context/github/GithubReducer.js
@@ -4,7 +4,12 @@ const GithubReducer = (state, action) => {
       return {
         ...state,
         users: action.payload,
-        loading: false
+        loading: false,
+      }
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: true,
       }
     default:
       return state
