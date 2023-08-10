d605a078c3ddb4f72758c9ecce4296532793c48d
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-10 19:51:27 +0200

    Display Users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 const UserResults = () => {
   const [loading, setLoading] = useState(true)
   const [users, setUsers] = useState([])
@@ -23,7 +24,10 @@ const UserResults = () => {
   if (!loading) { // if false
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md_grid-cols-2'>
-        {users.map((user) => (<h3>{user.login}</h3>))}
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        )
+        )}
       </div>
     )
   } else {
