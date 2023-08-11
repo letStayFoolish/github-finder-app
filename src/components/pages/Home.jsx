735e6813cca1565dd32dc9b8377c0cac53b047c3
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-11 18:01:30 +0200

    User Search Component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserResults from "../users/UserResults";
-
+import UserSearch from "../users/UserSearch";
 const Home = () => {
   return (
     <div>
       <h1>
+        <UserSearch />
         <UserResults />
       </h1>
     </div>
