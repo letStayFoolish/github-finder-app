b0e1dc5d167f6ccedd3cce25dc3795c01d473003
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-12 20:42:49 +0200

    Get Single User

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import {Routes, Route, BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
-import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
+import User from "./components/users/User";
+import NotFound from "./components/pages/NotFound";
 import {GithubProvider} from "./components/context/github/GithubContext";
 import {AlertProvider} from "./components/context/alert/AlertContext";
 import Alert from "./components/layout/Alert";
@@ -19,9 +20,10 @@ function App() {
             <main className='container mx-auto px-3 pb-12'>
               <Alert />
               <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/*' element={<NotFound/>}/>
+                <Route path='/' element={<Home />}/>
+                <Route path='/about' element={<About />}/>
+                <Route path='/users/:login' element={<User />}/>
+                <Route path='/*' element={<NotFound />}/>
               </Routes>
             </main>
             <Footer/>
--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -11,6 +11,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   }
 
@@ -25,18 +26,40 @@ export const GithubProvider = ({children}) => {
 
     setLoading()
 
-    const request = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
       }
     })
 
-    const {items} = await request.json()
+    const { items } = await response.json()
 
     dispatch({
       type: 'GET_USERS',
       payload: items,
     })
+  }
+
+  // Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
   }
 
   // Set loading
@@ -47,8 +70,10 @@ export const GithubProvider = ({children}) => {
     <GithubContext.Provider
       value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
         searchUsers,
+        getUser,
         clearUsers
       }
       }
