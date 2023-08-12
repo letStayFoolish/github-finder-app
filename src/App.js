277395b70ddc70838b6cfdef8596fb647e23b43a
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-12 17:03:57 +0200

    Alert Context & Reducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import { Routes, Route, BrowserRouter as Router} from "react-router-dom";
+import {Routes, Route, BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
 import {GithubProvider} from "./components/context/github/GithubContext";
+import {AlertProvider} from "./components/context/alert/AlertContext";
+
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar/>
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes >
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/*' element={<NotFound/>}/>
+              </Routes>
+            </main>
+            <Footer/>
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
 
   );
